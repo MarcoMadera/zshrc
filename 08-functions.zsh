192b44ea0ded5a0b8bc8e1063a3a1376cad9b793
Marco Madera <me@marcomadera.com>
2025-05-06 02:22:04 -0700

    remove duplicated function (#5)

--- 08-functions.zsh
+++ 08-functions.zsh
@@ -9,15 +9,6 @@ please() {
 
 # ━━━━━━━ File Manager ━━━━━━━━━
 y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
-}
-
-y() {
   local tmp cwd
   tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
   yazi "$@" --cwd-file="$tmp"
