59241f7980417ddca969fddad2f311b6117c2f68
MarcoMadera <me@marcomadera.com>
2026-05-02 17:52:03 -0700

    feat(function): java

--- 01-path.zsh
+++ 01-path.zsh
@@ -19,3 +19,6 @@ for p in "${local_paths[@]}"; do
 done
 
 export PATH
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
